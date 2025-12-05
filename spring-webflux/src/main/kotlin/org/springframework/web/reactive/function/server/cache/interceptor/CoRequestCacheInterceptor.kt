0f00eeef3c28e01a2a2f803d76943e9ad719dfcc
Angelo Bracaglia <angelo.bracaglia.ing@gmail.com>
2025-12-05 10:24:02 +0100

    Add warn log when CoRequestCaching is skipped
    Signed-off-by: Angelo Bracaglia <angelo.bracaglia.ing@gmail.com>

--- spring-webflux/src/main/kotlin/org/springframework/web/reactive/function/server/cache/interceptor/CoRequestCacheInterceptor.kt
+++ spring-webflux/src/main/kotlin/org/springframework/web/reactive/function/server/cache/interceptor/CoRequestCacheInterceptor.kt
@@ -18,6 +18,7 @@ package org.springframework.web.reactive.function.server.cache.interceptor
 
 import org.aopalliance.intercept.MethodInterceptor
 import org.aopalliance.intercept.MethodInvocation
+import org.apache.commons.logging.LogFactory
 import org.springframework.cache.interceptor.KeyGenerator
 import org.springframework.web.reactive.function.server.cache.context.CoRequestCacheContext
 import reactor.core.publisher.Flux
@@ -25,14 +26,23 @@ import reactor.core.publisher.Mono
 import kotlin.coroutines.Continuation
 import kotlin.reflect.jvm.jvmName
 
+private val logger = LogFactory.getLog(CoRequestCacheInterceptor::class.java)
+
 internal class CoRequestCacheInterceptor(private val keyGenerator: KeyGenerator) : MethodInterceptor {
 	override fun invoke(invocation: MethodInvocation): Any? {
 		val coRequestCache =
 			(invocation.arguments.lastOrNull() as? Continuation<*>)
 				?.context[CoRequestCacheContext.Key]?.cache
-				?: return invocation.proceed()
+				?: run {
+					if (logger.isWarnEnabled) {
+						logger.warn(
+							"Skip CoRequestCaching for ${invocation.method}: coroutine cache context not available"
+						)
+					}
+					return invocation.proceed()
+				}
 
-		val targetObject = invocation.getThis() ?: throw IllegalStateException()
+		val targetObject = checkNotNull(invocation.getThis())
 
 		val coRequestCacheKey = keyGenerator.generate(targetObject, invocation.method, *invocation.arguments)
 

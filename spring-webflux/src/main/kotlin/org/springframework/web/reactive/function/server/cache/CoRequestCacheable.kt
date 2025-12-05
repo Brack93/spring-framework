9780e42275aa93a0c2ee13e53e769cb74f9a0635
Angelo Bracaglia <angelo.bracaglia.ing@gmail.com>
2025-12-06 00:56:22 +0100

    Add CoRequestCacheable KDoc
    Signed-off-by: Angelo Bracaglia <angelo.bracaglia.ing@gmail.com>

--- spring-webflux/src/main/kotlin/org/springframework/web/reactive/function/server/cache/CoRequestCacheable.kt
+++ spring-webflux/src/main/kotlin/org/springframework/web/reactive/function/server/cache/CoRequestCacheable.kt
@@ -16,6 +16,52 @@
 
 package org.springframework.web.reactive.function.server.cache
 
+/**
+ * Annotation indicating that the result of invoking a *suspend method* can be cached
+ * for the lifespan of the underlying web request coroutine.
+ *
+ * Example:
+ *
+ * ```
+ * class MyServiceBean {
+ *
+ * 	@CoRequestCacheable(key = "#userName")
+ * 	suspend fun fetchUserAgeFromDownstreamService(userName: String, authHeader: String): Int {
+ * 		// prepare request and fetch the user info
+ * 		return userInfo.age
+ * 	}
+ *
+ * }
+ * ```
+ *
+ * Each time an advised suspend method is invoked, caching behavior will be applied,
+ * checking whether the method has been already invoked for the given arguments *within the same web request execution*.
+ * A sensible default simply uses the method parameters to compute the key, but
+ * a SpEL expression can be provided via the [key] attribute.
+ *
+ * If no value is found in the cache for the computed key, the target method
+ * will be invoked and the returned value will be stored in the coroutine context.
+ *
+ * Note that breaking
+ * [structured concurrency](https://kotlinlang.org/docs/coroutines-basics.html#coroutine-scope-and-structured-concurrency)
+ * by invoking the annotated method in a coroutine scope not tied to the web request, will prevent any caching behaviour.
+ *
+ * @author Angelo Bracaglia
+ * @since 7.0
+ * @see EnableCoRequestCaching
+ */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
-annotation class CoRequestCacheable(val key: String = "")
+annotation class CoRequestCacheable(
+
+	/**
+	 * Spring Expression Language (SpEL) expression for computing the key dynamically.
+	 *
+	 * The default value is `""`, meaning all method parameters are considered as a key.
+	 *
+	 * Method arguments can be accessed by index. For instance the second argument
+	 * can be accessed via `#p1` or `#a1`.
+	 * Arguments can also be accessed by name if that information is available.
+	 */
+	val key: String = ""
+)
